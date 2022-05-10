5f7d77d95ef2e816238908f7ad8cc9967d9fbdc6
SSemenkin <linkforptr@gmail.com>
2022-05-10 08:48:53 +0300

    выа

--- sqlexecutor.cpp
+++ sqlexecutor.cpp
@@ -1,104 +1,184 @@
 #include "sqlexecutor.h"
-#include <QSqlField>
 #include <QSqlQuery>
+#include <QThread>
 #include <QSqlError>
+#include <QSqlField>
 #include <QSqlRecord>
 
 
-
-SqlExecutor::SqlExecutor(const QString &driver,
-                         const QString &connection,
-                         const QString &hostname,
-                         const QString &database,
-                         const QString &username,
-                         const QString &password,
-                         uint port) :
-      m_driver(driver),
-      m_connection(connection),
-      m_hostname(hostname),
-      m_databasename(database),
-      m_username(username),
-      m_password(password),
-      m_port(port),
-      m_thread(new QThread),
-      m_ExecutorPool(SqlExecutorPool::instance())
+SqlExecutor::SqlExecutor() noexcept
 {
-    m_ExecutorPool->append(this);
-    moveToThread(m_thread.data());
-    connect(m_thread.data(), &QThread::started, this, &SqlExecutor::executeQuery);
+    initThread();
 }
 
-SqlExecutor::~SqlExecutor()
+SqlExecutor::SqlExecutor(const QString &driver, const QString &connectionName,
+                         const QString &hostname, const QString &databaseName,
+                         const QString &username, const QString &password,
+                         uint32_t port) noexcept :
+    m_driver(driver)
+  , m_connectionName(connectionName)
+  , m_hostname(hostname)
+  , m_databaseName(databaseName)
+  , m_username(username)
+  , m_password(password)
+  , m_port(port)
 {
-    while(!thread()->isFinished());
-    m_ExecutorPool->remove(this);
+    initThread();
 }
 
-void SqlExecutor::setQuery(const QString &query)
+SqlExecutor::~SqlExecutor() noexcept
 {
+    if (!m_thread->isFinished()) {
+        m_thread->quit();
+        m_thread->wait();
+    }
+}
+
+QString SqlExecutor::driver() const noexcept
+{
+    return m_driver;
+}
+
+QString SqlExecutor::connectionName() const noexcept
+{
+    return m_connectionName;
+}
+
+QString SqlExecutor::hostname() const noexcept
+{
+    return m_hostname;
+}
+
+QString SqlExecutor::databaseName() const noexcept
+{
+    return m_databaseName;
+}
+
+QString SqlExecutor::username() const noexcept
+{
+    return m_username;
+}
+
+QString SqlExecutor::password() const noexcept
+{
+    return m_password;
+}
+
+uint32_t SqlExecutor::port() const noexcept
+{
+    return m_port;
+}
+
+void SqlExecutor::setDriver(const QString &driver) noexcept
+{
+    m_driver = driver;
+}
+
+void SqlExecutor::setConnectionName(const QString &connectionName) noexcept
+{
+    m_connectionName = connectionName;
+}
+
+void SqlExecutor::setHostname(const QString &hostname) noexcept
+{
+    m_hostname = hostname;
+}
+
+void SqlExecutor::setDatabaseName(const QString &databaseName) noexcept
+{
+    m_databaseName = databaseName;
+}
+
+void SqlExecutor::setUsername(const QString &username) noexcept
+{
+    m_username = username;
+}
+
+void SqlExecutor::setPassword(const QString password) noexcept
+{
+    m_password = password;
+}
+
+void SqlExecutor::setPort(uint32_t port) noexcept
+{
+    m_port = port;
+}
+
+void SqlExecutor::executeQuery(const QString &query) noexcept
+{
+    if (m_thread->isRunning()) {
+        return;
+    }
     m_query = query;
+    m_thread->start();
 }
 
-const QString &SqlExecutor::query() const
+QString SqlExecutor::query() const noexcept
 {
     return m_query;
 }
 
-void SqlExecutor::exec()
+bool SqlExecutor::connectToDatase() noexcept
 {
-    thread()->start();
+   if (QSqlDatabase::contains(m_databaseName)) {
+       m_database = QSqlDatabase::database(m_connectionName);
+       return m_database.open();
+   }
+
+   m_database = QSqlDatabase::addDatabase(m_driver, m_connectionName);
+   m_database.setDatabaseName(m_databaseName);
+   m_database.setHostName(m_hostname);
+   m_database.setUserName(m_username);
+   m_database.setPassword(m_password);
+   m_database.setPort(m_port);
+   return m_database.open();
 }
 
-bool SqlExecutor::connectToDatabase()
+void SqlExecutor::execute() noexcept
 {
-    if (QSqlDatabase::contains(m_connection)) {
-        m_database = QSqlDatabase::database(m_connection);
-    } else {
-        m_database = QSqlDatabase::addDatabase(m_driver, m_connection);
-        m_database.setDatabaseName(m_databasename);
-        m_database.setHostName(m_hostname);
-        m_database.setUserName(m_username);
-        m_database.setPassword(m_password);
-        m_database.setPort(m_port);
+    if (!m_database.isOpen()) {
+        bool isConnected = connectToDatase();
+        if (!isConnected) {
+            emit error(m_database.lastError());
+            return;
+        }
     }
-    return m_database.open();
-}
 
-void SqlExecutor::executeQuery()
-{
-    if (connectToDatabase()) {
+    QSqlQuery query(m_query, m_database);
+    if (query.exec()) {
+        if (query.isSelect()) {
+            StringVector headers;
+            SqlResult data;
 
-        QSqlQuery query(m_database);
-
-        if (query.exec(m_query)) {
-
-            QStringList fieldnames;
-            QVector<QVariantList> data;
-            QVariantList row;
-
+            VariantVector record;
             while(query.next()) {
-
-                if (fieldnames.isEmpty()) {
+                if (headers.isEmpty()) {
                     for (int i = 0; i < query.record().count(); ++i) {
-                        fieldnames.push_back(query.record().fieldName(i));
+                        headers.push_back(query.record().fieldName(i));
                     }
                 }
 
-
                 for (int i = 0; i < query.record().count(); ++i) {
-                    row << query.value(i);
+                    record.push_back(query.record().value(i));
                 }
-
-                data.push_back(row);
-                row.clear();
-
+                data.push_back(record);
+                record.clear();
             }
-            emit executed(fieldnames, data);
+
+            emit finished(headers, data);
         } else {
-            emit error(query.lastError().text());
+            emit finished(std::nullopt, std::nullopt);
         }
     } else {
-        emit error(m_database.lastError().text());
+        emit error(query.lastQuery());
     }
+
     m_thread->quit();
+}
+
+void SqlExecutor::initThread()
+{
+    m_thread.reset(new QThread);
+    moveToThread(m_thread.data());
+    connect(m_thread.data(), &QThread::started, this, &SqlExecutor::execute);
 }
--- sqlexecutor.h
+++ sqlexecutor.h
@@ -1,51 +1,65 @@
 #ifndef SQLEXECUTOR_H
 #define SQLEXECUTOR_H
-
-#include "sqlexecutorpool.h"
+#include <QObject>
 #include <QSqlDatabase>
 
+class QThread;
 
-
-class SQLEXECUTOR_EXPORT SqlExecutor : public QObject
+class SqlExecutor final : public QObject
 {
     Q_OBJECT
+    using VariantVector = QVector<QVariant>;
+    using SqlResult = QVector<VariantVector>;
+    using StringVector = QVector<QString>;
 public:
-    explicit SqlExecutor (const QString &driver,
-                          const QString &connection,
-                          const QString &hostname,
-                          const QString &database,
-                          const QString &username,
-                          const QString &password,
-                          uint port);
-    virtual ~SqlExecutor();
+    explicit SqlExecutor() noexcept;
+    explicit SqlExecutor(const QString &driver, const QString &connectionName,
+                         const QString &hostname, const QString &databaseName,
+                         const QString &username, const QString &password,
+                         uint32_t port) noexcept;
+    ~SqlExecutor() noexcept;
 
-    void setQuery(const QString &query);
-    const QString &query() const;
+    [[nodiscard]] QString driver () const noexcept;
+    [[nodiscard]] QString connectionName() const noexcept;
+    [[nodiscard]] QString hostname() const noexcept;
+    [[nodiscard]] QString databaseName() const noexcept;
+    [[nodiscard]] QString username() const noexcept;
+    [[nodiscard]] QString password() const noexcept;
+    [[nodiscard]] uint32_t port() const noexcept;
 
-    void exec();
+    void setDriver(const QString &driver) noexcept;
+    void setConnectionName(const QString &connectionName) noexcept;
+    void setHostname(const QString &hostname) noexcept;
+    void setDatabaseName(const QString &databaseName) noexcept;
+    void setUsername(const QString &username) noexcept;
+    void setPassword(const QString password) noexcept;
+    void setPort(uint32_t port) noexcept;
+
+    void executeQuery(const QString &query) noexcept;
+    QString query() const noexcept;
 
 signals:
-    void error(const QString &errorText);
-    void executed(const QStringList &fieldnames, const QVector<QVariantList> &data);
+    void finished(const std::optional<StringVector> &headers,
+                  const std::optional<SqlResult> &result);
+    void error(const QSqlError &error);
 
-protected:
+private:
+    bool connectToDatase() noexcept;
+    void execute() noexcept;
+    void initThread();
+private:
     QString m_driver;
-    QString m_connection;
+    QString m_connectionName;
     QString m_hostname;
-    QString m_databasename;
+    QString m_databaseName;
     QString m_username;
     QString m_password;
-    uint m_port;
+    uint32_t m_port;
 
-private:
-    virtual bool connectToDatabase();
-    void executeQuery();
-
-private:
     QString m_query;
-    QScopedPointer<QThread> m_thread;
+
     QSqlDatabase m_database;
-    SqlExecutorPool *m_ExecutorPool;
+    QScopedPointer<QThread> m_thread;
 };
 
 #endif // SQLEXECUTOR_H
